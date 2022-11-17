1b7a5a1869d25437d3ee0679031a5c5aa3d9ea78
Ubuntu <ubuntu@ip-172-31-78-201.ec2.internal>
2022-11-17 20:24:38 +0000

    added register and login views

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,10 @@
 class StaticController < ApplicationController
   def Home
   end
+  
+  def Register
+  end
+  
+  def Login
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'static#home'
+  get '/register', to: 'static#register'
+  get '/login', to: 'static#login'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
