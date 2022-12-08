ef6f55a67f0a9724a63859ed0a99d268e511c3bd
Ubuntu <ubuntu@ip-172-31-78-201.ec2.internal>
2022-12-08 01:30:18 +0000

    added sign out button on feed

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,7 @@ class StaticController < ApplicationController
   #before_action :authenticate_user!
   def Home
   end
+  
+  def Feed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   #get '/register', to: 'static#register'
   #get '/login', to: 'static#login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/feed', to: 'static#feed'
   # Defines the root path route ("/")
   # root "articles#index"
+#  namespace :user do
+ #   root :to => "static#feed"
+#  end
 end
